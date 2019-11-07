29dec601e9c3a5ab0c5c6ec8f7c0cf398e320adc
kashansk <kashansk@gmail.com>
2019-11-07 15:07:16 +0500

    Verified list Evidences Added

---
+++ src/Data/ApplicationRole.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BES.Data
+{
+    public class ApplicationRole : IdentityRole
+    {
+        public ICollection<ApplicationUserRole> UserRoles { get; set; }
+    }
+}
